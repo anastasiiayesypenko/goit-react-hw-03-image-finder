e9990e172d59f5c267f9f6e8963cf4d4ec2780df
Anastasia <nastiyayesypenko@ukr.net>
2019-07-12 23:49:48 +0300

    Updates

--- precache-manifest.ea6be4270ee6c9005982c885356ddc2f.js
+++ precache-manifest.ea6be4270ee6c9005982c885356ddc2f.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a2da1da595e9b9f786fe6c0e074437e2",
+    "revision": "fe0a4392f41c4e63b8ca128d88441ad4",
     "url": "/goit-react-hw-03-image-finder/index.html"
   },
   {
-    "revision": "a7602bb177908dd68d55",
+    "revision": "5ae981ccf2242a69312b",
     "url": "/goit-react-hw-03-image-finder/static/css/main.96fd6ac2.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-03-image-finder/static/js/2.5a808aba.chunk.js"
   },
   {
-    "revision": "a7602bb177908dd68d55",
-    "url": "/goit-react-hw-03-image-finder/static/js/main.e6979472.chunk.js"
+    "revision": "5ae981ccf2242a69312b",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.21fd9f69.chunk.js"
   },
   {
     "revision": "052338c15f0cfa810fb3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-03-image-finder/precache-manifest.41d17b8aec6a5e411fccbdda048fbd75.js"
+  "/goit-react-hw-03-image-finder/precache-manifest.ea6be4270ee6c9005982c885356ddc2f.js"
 );
 
 self.addEventListener('message', (event) => {
